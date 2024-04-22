88379108f525f5815ca623866f39e10c6b138a34
Heitor0308 <heitorferrari381@gmail.com>
2024-04-22 20:46:45 -0300

    first commit

---
+++ AMST4.Carousel.MVC/Context/AplicationDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AMST4.Carousel.MVC.Models;
+
+namespace AMST4.Carousel.MVC.Context
+{
+    public class AplicationDataContext : DbContext
+    {
+        public AplicationDataContext(DbContextOptions<AplicationDataContext> options) : base(options) { }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ AMST4.Carousel.MVC/Views/Shared/_Carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AMST4.Carousel.MVC.Views.Shared
+{
+    public class _CarouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
